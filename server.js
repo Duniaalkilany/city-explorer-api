fbc6114af041f68559139cea07405c4eab0fe7c0
Duniaalkilany <duniaalkilany908@gmail.com>
2021-06-30 07:21:51 +0300

    finished lab 7

--- server.js
+++ server.js
@@ -1,10 +1,73 @@
 const express = require('express') // require the express package
+const weather=require('./data/weather.json')
 const app = express() // initialize your express app instance
- 
+require('dotenv').config();//before const PORT
+
+const PORT = process.env.PORT ;//this is so it does not break in heroku build out
+const cors = require('cors');
+app.use(cors()) 
+
+
+
 // a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
- 
-app.listen(8000) // kick start the express server to work
+
+app.get('/', (req, res) =>{
+   res.send('Hello World')});
+
+app.get('/weather', (req, res) =>{ 
+
+let lat =req.query.lat
+ let lon = req.query.lon
+let searchQuery=req.query.searchQuery
+console.log(lat);
+console.log(lon);
+console.log(searchQuery);
+try{
+ let findData=()=>{
+
+  let city =weather.find((city,idx)=>{
+    return city.city_name.toLowerCase()===searchQuery.toLowerCase() && city.lat===Number(lat) && city.lon==Number(lon)})
+
+ return city.data.map(item=>{
+   return new ForeCast(item)
+ })}
+
+ res.json( findData())
+   
+}catch(err){
+
+res.status(500)
+res.send('Error :something went wrong !!')
+
+
+
+}
+
+ });
+
+
+
+
+
+
+
+
+
+
+
+ class ForeCast {
+   constructor(weathInfo){
+    this.date=weathInfo.valid_date;
+    this.description=weathInfo.weather.description
+    // this.city_name=weathInfo.city_name
+  }
+};
+
+
+
+
+app.listen(PORT, () => 
+{console.log(`server is listening on port ${PORT}`)
+console.log(PORT);})
+
+// && city.lat===Number(lat) && city.lon===Number(lon) 
